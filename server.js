8a33d031d56ffe11a25027342bfff0fcc270517d
sukhvinder17 <123041876+sukhvinder17@users.noreply.github.com>
2023-01-29 22:18:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: ______Sukhvinder________________ Student ID: ___159790211___________ Date: ___20/01/2023_____________
+*  Cyclic Link: ________https://doubtful-tiara-colt.cyclic.app_______________________________________________________
+*
+********************************************************************************/ 
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -5,95 +17,15 @@ const dotenv = require('dotenv').config();
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
-
+const mongoose = require('mongoose')
 const MoviesDB = require("./modules/moviesDB.js");
 const db = new MoviesDB();
 
 
 
-//support incoming JSON entities
+
 
 app.use(express.json());
-
-// Installed packages 
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const app = express()
-
-require('dotenv').config({path: "./keys.env"}); 
-const { MONGODB_CONN_STRING } = process.env;
-
-// Required for MoviesDB module
-const MoviesDB = require("./modules/moviesDB.js");
-const db = new MoviesDB();
-
-const HTTP_PORT = process.env.PORT || 8080;
-
-// using JSON routes
-app.use(express.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
-    app.listen(HTTP_PORT, ()=>{
-        console.log(`server listening on: ${HTTP_PORT}`);
-    });
-}).catch((err)=>{
-    console.log(err);
-});
-
-
-app.get("/", (req, res) => {
-    res.json({message: "API Listening"});
-})
-app.post("/api/movies", (req, res)=>{
-    db.addNewMovie(req.body).then((data)=>{
-        res.status(201).json(data);
-    }).catch((err)=>{
-       res.json({ message: err });
-    });
- });
- 
- app.get("/api/movies", (req, res)=>{
-    db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((data)=>{
-        res.json(data);
-    }).catch((err)=>{
-        res.status(404).json({ message: err });
-    });
- });
- 
- app.get("/api/movies/:id", (req, res)=>{
-    const id = req.params.id;
-    db.getMovieById(id).then((data)=>{
-        res.json(data);
-    })
-    .catch((err)=>{
-        const msg = 'Cannot find Movie id: ${id} in the database. ERROR: ${err}';
-        console.log(msg);
-        res.status(204).json(msg);
-    });
- });
- 
- app.put("/api/movies/:id", (req, res)=>{
-   const id = req.params.id;
-    db.updateMovieById(req.body, id).then(()=>{
-       res.send(`${req.body.title} updated.`);
-    }).catch((err)=>{
-       res.status(500).send('Cannot update Movie. ERROR: ${err}');
-    });
- });
- 
- app.delete("/api/movies/:id", (req, res)=>{
-    const id = req.params.id;
-    db.deleteMovieById(id).then(()=>{
-       res.send('Movie id: ${id} deleted');
-    }).catch((err)=>{
-       res.status(204).send('Cannot find Movie id: ${id} in the database. ERROR: ${err}');
-    });
- });
 app.use(cors());
 
 
@@ -113,17 +45,17 @@ app.post('/api/movies', (req, res) => {
   
   app.get('/api/movies', (req, res) => {
       db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((data) => {
-        if (data.length === 0) res.status(204).json({ message: "No data returned"});  // 204 status code it's no content
-        else res.status(201).json(data);  // 201 status code it's when is created
+        if (data.length === 0) res.status(204).json({ message: "No data returned"});  
+        else res.status(201).json(data);  
       }).catch((err) => {
-        res.status(500).json({ error: err });  // 500 to show msg internal server error
+        res.status(500).json({ error: err }); 
       })
   });
   
-  // this route to accepting a id
+ 
   app.get('/api/movies/:_id', (req, res) => {
     db.getMovieById(req.params._id).then((data) => {
-      res.status(201).json(data)  // when it accepts should be code status 201 that proved data has crated
+      res.status(201).json(data)  
     }).catch((err) => {
       res.status(500).json({ error: err });
     })
@@ -143,7 +75,7 @@ app.post('/api/movies', (req, res) => {
   
   app.delete('/api/movies/:_id', async (req, res) => {
     db.deleteMovieById(req.params._id).then(() => {
-      res.status(202).json({ message: `The ${req.params._id} removed from the system`})  // 202 status code accepted to delete the movie
+      res.status(202).json({ message: `The ${req.params._id} removed from the system`})  
       .catch((err) => {
         res.status(500).json({ error: err })
       })
