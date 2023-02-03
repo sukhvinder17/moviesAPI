2af6e42977f37d134db76602e1783dbe05945c1e
sukhvinder17 <123041876+sukhvinder17@users.noreply.github.com>
2023-02-03 17:04:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,90 +1,85 @@
 
-/*********************************************************************************
-*  WEB422 – Assignment 1
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: ______Sukhvinder________________ Student ID: ___159790211___________ Date: ___20/01/2023_____________
-*  Cyclic Link: ________https://doubtful-tiara-colt.cyclic.app_______________________________________________________
-*
-********************************************************************************/ 
 
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
-const dotenv = require('dotenv').config();
+const bodyParser = require("body-parser");
+const dataService = require("./modules/data-service.js");
+
+const myData = dataService("mongodb+srv://test-user:test123@cluster0.62esx.mongodb.net/sample_supplies?retryWrites=true&w=majority");
 
 const app = express();
-const HTTP_PORT = process.env.PORT || 8080;
-const mongoose = require('mongoose')
-const MoviesDB = require("./modules/moviesDB.js");
-const db = new MoviesDB();
 
-
-
-
-
-app.use(express.json());
 app.use(cors());
 
+app.use(bodyParser.json());
 
+const HTTP_PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.json({message: "API Listening"});
+// ************* API Routes
+
+// POST /api/sales (NOTE: This route must read the contents of the request body)
+app.post ("/api/sales", (req,res) => {
+    myData.addNewSale(req.body)
+    .then(() => {
+        res.status(201).json(`new sales has been added`);
+    })
+    .catch((err)=> {
+        res.status(400).json(err);
+    });
 })
 
-app.post('/api/movies', (req, res) => {
-    if(Object.keys(req.body).length === 0) {
-      res.status(500).json({ error: "Invalid number "});
-    } else {
-      db.addNewMovie(req.body).then((data) => { res.status(201).json(data)
-      }).catch((err) => { res.status(500).json({ error: err }); });
-    }
-  });
-  
-  app.get('/api/movies', (req, res) => {
-      db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((data) => {
-        if (data.length === 0) res.status(204).json({ message: "No data returned"});  
-        else res.status(201).json(data);  
-      }).catch((err) => {
-        res.status(500).json({ error: err }); 
-      })
-  });
-  
- 
-  app.get('/api/movies/:_id', (req, res) => {
-    db.getMovieById(req.params._id).then((data) => {
-      res.status(201).json(data)  
-    }).catch((err) => {
-      res.status(500).json({ error: err });
-    })
-  })
-  
-  app.put('/api/movie/:_id', async (req, res) => {
-    try {
-      if (Object.keys(req.body).length === 0) {
-        return res.status(500).json({ error: "No data to update"});
-      }
-      const data = await db.updateMovieById(req.body, req.params._id);
-      res.json({ success: "Movie updated!"});
-    }catch(err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
-  
-  app.delete('/api/movies/:_id', async (req, res) => {
-    db.deleteMovieById(req.params._id).then(() => {
-      res.status(202).json({ message: `The ${req.params._id} removed from the system`})  
-      .catch((err) => {
-        res.status(500).json({ error: err })
-      })
-    })
-  });
-  
 
-db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
-    app.listen(HTTP_PORT, ()=>{
+// GET /api/sales (NOTE: This route must accept the numeric query parameters "page" and "perPage", ie: /api/sales?page=1&perPage=5 )
+app.get("/api/sales", (req,res) => {
+    myData.getAllSales(req.query.page, req.query.perPage)
+    .then((sales) => {
+        res.status(200).json(sales);
+    })
+    .catch((err)=> {
+        res.status(400).json(err);
+    });
+})
+
+
+// GET /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
+app.get("/api/sales/:id", (req,res) => {
+    myData.getSaleById(req.params.id)
+    .then((sales) => {
+        res.status(200).json(sales);
+    })
+    .catch((err)=> {
+        res.status(400).json(err);
+    });
+})
+
+
+// PUT /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8 as well as read the contents of the request body)
+app.put("/api/sales/:id", (req,res) => {
+    myData.updateSaleById(req.body, req.params.id)
+    .then(()=> {
+        res.status(200).json(`sale ${req.body._id} sucessfully updated`);
+    })
+    .catch((err)=> {
+        res.status(404).json(err);
+    });
+})
+
+
+// DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
+app.delete("/api/sales/:id", (req,res)=> {
+    myData.deleteSaleById(req.params.id)
+    .then(()=> {
+        res.status(200).json(`sale ${req.body._id} sucessfully deleted`);
+    })
+    .catch((err)=> {
+        res.status(404).json(err);
+    });
+})
+
+// ************* Initialize the Service & Start the Server
+
+myData.initialize().then(()=>{
+    app.listen(HTTP_PORT,()=>{
         console.log(`server listening on: ${HTTP_PORT}`);
     });
 }).catch((err)=>{
