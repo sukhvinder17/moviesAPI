b66dd975955224fb5431b953d2ed1f197235d98a
sukhvinder17 <123041876+sukhvinder17@users.noreply.github.com>
2023-02-24 12:47:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,82 +13,80 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const dotenv = require('dotenv').config();
-
-const app = express();
-const HTTP_PORT = process.env.PORT || 8080;
+require('dotenv').config();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const MoviesDB = require("./modules/moviesDB.js");
 const db = new MoviesDB();
-db.initialize(process.env.MONGODB_CONN_STRING)
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
 
-app.listen(process.env.PORT||3000)
 
-app.get('/', (req, res) => {
-    res.json({message : "API Listening"});
-});
+
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+    res.json({message: "API Listening"});
+})
 
 app.post('/api/movies', (req, res) => {
-    db.addNewMovie(req.body).then(()=>{
-        res.status(200).send("New Movie Added Successfully");
-    }).catch((err)=>{
-        res.status(500).send(`Movie Wasnt added successfully : ` + {err});
+    if(Object.keys(req.body).length === 0) {
+      res.status(500).json({ error: "Invalid number "});
+    } else {
+      db.addNewMovie(req.body).then((data) => { res.status(201).json(data)
+      }).catch((err) => { res.status(500).json({ error: err }); });
+    }
+  });
+  
+  app.get('/api/movies', (req, res) => {
+      db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((data) => {
+        if (data.length === 0) res.status(404).json({ message: "No data returned"});  
+        else res.status(201).json(data);  
+      }).catch((err) => {
+        res.status(500).json({ error: err }); 
+      })
+  });
+  
+ 
+  app.get('/api/movies/:_id', (req, res) => {
+    db.getMovieById(req.params._id).then((data) => {
+      res.status(201).json(data)  
+    }).catch((err) => {
+      res.status(500).json({ error: err });
     })
-});
-
-app.get('/api/movies', (req, res) => {
-    db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((data)=>{
-        if(data.length == 0)
-        {
-            res.status(404).json({error : "Unable to find requested movie"});
-        }
-        else
-        {
-            res.status(200).json(data);
-        }
-    }).catch((err)=>{
-        res.status(500).send('Unable to return movies : ' + {err});
+  })
+  
+  app.put('/api/movie/:_id', async (req, res) => {
+    try {
+      if (Object.keys(req.body).length === 0) {
+        return res.status(500).json({ error: "No data to update"});
+      }
+      const data = await db.updateMovieById(req.body, req.params._id);
+      res.json({ success: "Movie updated!"});
+    }catch(err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+  
+  app.delete('/api/movies/:_id', async (req, res) => {
+    db.deleteMovieById(req.params._id).then(() => {
+      res.status(202).json({ message: `The ${req.params._id} removed from the system`})  
+      .catch((err) => {
+        res.status(500).json({ error: err })
+      })
     })
-});
-
-app.get('/api/movies/:_id', (req, res) => {
-    db.getMovieById(req.params._id).then((data)=>{
-        res.status(200).json(data);
-    }).catch((err)=>{
-        res.status(500).send(`Unable to return movie by requested id : ` + {err});
-    })
-});
-
-app.put('/api/movies/:_id', (req, res) => {
-    db.updateMovieById(req.body, req.params._id).then(()=>{
-        res.status(200).send(`Updated the requested movie of id : ` + req.params._id);
-    }).catch((err)=>{
-        res.status(500).send(`Unable to update movie by requested id : ` + {err});
-    })
-});
-
-
-app.delete('/api/movies/:_id', (req, res) => {
-    db.deleteMovieById(req.params._id)
-    .then(()=>{
-        res.status(200).send(`Deleted the requested movie of id : ` + req.params._id);
-    })
-    .catch((err)=>{
-        res.status(500).send(`Unable to delete movie by requested id : ` + {err});
-    })
-});
-
-
+  });
+  
 
 db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
     app.listen(HTTP_PORT, ()=>{
         console.log(`server listening on: ${HTTP_PORT}`);
-        //console.log(process.env.MONGODB_CONN_STRING);
     });
 }).catch((err)=>{
     console.log(err);
 });
-
-
-
-
